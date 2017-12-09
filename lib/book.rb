a7ea79d63150693d3cc6f675e56b71dc3acf4942
James Ferguson <ncjamesferguson@gmail.com>
2017-12-09 02:07:19 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,8 @@
 class Book
 
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :title, :author, :page_count, :genre
 
-  def intialize(title)
+  def initialize(title)
     @title = title
   end
 
